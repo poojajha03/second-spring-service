4de20317922f09e0f18b9deb24175899f74d16cd
vpoojabhosale <v.pooja.bhosale@red.virgin.com>
2023-10-11 15:48:24 +0100

    https://github.com/poojajha03/second-spring-service

---
+++ src/main/java/com/spring/service/secondspringservice/SecondSpringServiceApplication.java
@@ -0,0 +1,13 @@
+package com.spring.service.secondspringservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondSpringServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SecondSpringServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/spring/service/secondspringservice/SecondSpringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.service.secondspringservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondSpringServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
